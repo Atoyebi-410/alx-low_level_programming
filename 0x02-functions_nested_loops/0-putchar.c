648ff4085fd0ab45d23cc31745481707a014093b
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2023-07-18 17:26:37 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,17 @@
 *
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-int i;
-char p[] = "_putchar";
-
-for (i = 0; i < 9; i++)
-{
-_putchar(p[i]);
-}
+_putchar('-');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
