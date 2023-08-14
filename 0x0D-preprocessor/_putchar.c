ef9c4997e1d50eca843e08914886edb93f2c5bb9
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2023-08-14 11:23:53 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+
+/**
+ * _putchar - this writes the char c to stdout
+ * @c: The char to print
+ *
+ * Return: 1 on success.
+ * On error, return -1, and errno is set appropriately.
+ */
+
+
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
