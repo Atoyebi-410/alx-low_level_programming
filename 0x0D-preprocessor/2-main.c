b54f51c793adf07a61e9c3bf3aa78d1d782a1ff4
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2023-08-14 11:39:04 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - this prints name of compilation file with new line
+ * Return: nothing
+ **/
+
+
+int main(void)
+
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
