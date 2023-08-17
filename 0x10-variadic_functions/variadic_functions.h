802af92215816cb3a5159a4ef3acd3b4f2a2db56
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2023-08-17 08:02:36 -0700

    3-print_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
-#ifndef VARFUNC
-#define VARFUNC
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
