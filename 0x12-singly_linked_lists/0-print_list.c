72098c8155b1b1cb239914664611112e05bc0bb5
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2023-08-24 16:49:37 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - this is to print list of elements
+ * @h: this is an input
+ * Return: this return number of nodes
+ */
+
+
+size_t print_list(const list_t *h)
+{
+  if (h)
+    {
+      if (h->str)
+	printf("[%d] %s%s", h->len, h->str, "\n");
+      else
+	printf("[%d] %s%s", 0, "(nil)", "\n");
+      if (h->next)
+	{
+	  return (1 + print_list(h->next));
+	}
+      return (1);
+    }
+  return (0);
+}
